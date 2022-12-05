47c6868ffa940fb246e103759b53eb1507a4a2d7
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-12-05 16:54:02 +0800

    style: 引入SCSS宽高度固定变量，在app.vue中引入宽高比例

--- src/App.vue
+++ src/App.vue
@@ -42,9 +42,11 @@ export default {
   font-family: 'HarmonyOS_Sans_SC_Regular', 'HarmonyOS_Sans_SC_Medium', 'HarmonyOS_Sans_SC_Thin', 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  --heightRate: calc(100vh / #{$base-height});
+  --widthRate: calc(100vw / #{$base-width});
   text-align: center;
   color: #2c3e50;
-    background-color: #e8e2e0;
+  background-color: #e8e2e0;
 }
 
 </style>
