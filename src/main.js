43bc0b12befa9b4b8fd61b36d234fcc6392f00d3
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-12-06 17:31:10 +0800

    feat: 初步使用mavon-editor

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'//引入axios
 
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+Vue.component("mavon-editor", mavonEditor);
+
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 Vue.use(axios);//应用axios
