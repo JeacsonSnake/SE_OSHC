0d353a3539e7ffb1a41c1ef0e5536d078dd370bc
Your Name <you@example.com>
2022-12-22 00:07:50 +0800

    write code in the PostSearchLayer

--- src/views/SearchPage/PostsSearchLayer.vue
+++ src/views/SearchPage/PostsSearchLayer.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="postsSearchLayer">
-   <div v-for="(item,index) in getSearchData" :key="idnex">
-  {{item}}
-   </div>
+    <div class="search_section_background">
+      <!-- searchSection -->
+      <p class="search_section_topic">
+        {{ searchSection.topic }}
+      </p>
+      <div class="search_section_button">点击进入</div>
+      <div class="search_section">
+        <!-- left -->
+        <div class="search_section_left">
+         <h2>{{searchSection.sectionLable}}</h2>
+         <p id="search_section_left_posts">贴子数：{{searchSection.posts}}</p>
+         <p id="search_section_left_hots">热度：{{searchSection.hots}}</p>
+         <p id="search_section_left_news">最新动态：{{searchSection.news}}</p>
+        </div>
+        <!-- right -->
+        <div class="search_section_right">
+          <img src="../../assets/images/tag_example/esp32.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+    <!-- {{searchList}} -->
+    <div v-for="(item, index) in getSearchData" :key="idnex">
+      <div class="searchList_item">
+        <!-- itemTopic -->
+        <div class="item_topic">{{ item.topic }}   
+          <img src="../../assets/images/label_head/精华_w30px.png" />
+        </div>
+        <!-- itemLabel -->
+        <div class="item_label">
+          <div>#{{item.postLabel}}</div>
+          <div>{{item.user}}</div>
+          <div>{{item.date}}</div>
+        </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -13,17 +45,49 @@ export default {
       circleUrl:
         "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
       //   value: false,
-      
-      searchSection:[""],
+
+      searchSection: {
+        topic: "这里有相关搜索内容的子版块，是否进入?",
+        sectionLable: "ARDUINO",
+        posts: "114514",
+        hots: "1919810",
+        news: "666",
+      },
+
       getSearchData: [
-        {user:"RICK_A",date:"三天前",postLabel:"#ESP32",views:"114514",
-        topic:"[新人必备] ESP32是个啥?全网最详细介绍ESP32!",brief:"ESP32DDDDDDDDDDDDDDDDDD"},
-        {user:"RICK_b",date:"三天前",postLabel:"#ESP32",views:"1919810",
-        topic:"[新人必备] ESP32是个啥?全网最详细介绍ESP32!",brief:"ESP32DDDDDDDDDDDDDDDDDD"},
-        {user:"RICK_c",date:"三天前",postLabel:"#ESP32",views:"114514",
-        topic:"[新人必备] ESP32是个啥?全网最详细介绍ESP32!",brief:"ESP32DDDDDDDDDDDDDDDDDD"},
-        {user:"RICK_d",date:"三天前",postLabel:"#ESP32",views:"114514",
-        topic:"[新人必备] ESP32是个啥?全网最详细介绍ESP32!",brief:"ESP32DDDDDDDDDDDDDDDDDD"},],
+        {
+          user: "RICK_A",
+          date: "三天前",
+          postLabel: "ESP32",
+          views: "114514",
+          topic: "[新人必备] ESP32是个啥?全网最详细介绍ESP32!",
+          brief: "ESP32DDDDDDDDDDDDDDDDDD",
+        },
+        {
+          user: "RICK_b",
+          date: "三天前",
+          postLabel: "ESP32",
+          views: "1919810",
+          topic: "[新人必备] ESP32是个啥?全网最详细介绍ESP32!",
+          brief: "ESP32DDDDDDDDDDDDDDDDDD",
+        },
+        {
+          user: "RICK_c",
+          date: "三天前",
+          postLabel: "ESP32",
+          views: "114514",
+          topic: "[新人必备] ESP32是个啥?全网最详细介绍ESP32!",
+          brief: "ESP32DDDDDDDDDDDDDDDDDD",
+        },
+        {
+          user: "RICK_d",
+          date: "三天前",
+          postLabel: "#ESP32",
+          views: "114514",
+          topic: "[新人必备] ESP32是个啥?全网最详细介绍ESP32!",
+          brief: "ESP32DDDDDDDDDDDDDDDDDD",
+        },
+      ],
       isEmpty: false,
     };
   },
@@ -168,14 +232,14 @@ export default {
 }
 
 .user {
-  height:  calc(100vh / 1080px * 25px);
+  height: calc(100vh / 1080px * 25px);
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
 #userName {
-  width:calc(1536px / 1920px * 100px);
+  width: calc(1536px / 1920px * 100px);
   min-width: 0;
   height: 100%;
   margin: 0 20px;
@@ -187,4 +251,140 @@ export default {
 .goodsPrise {
   color: rgb(240, 65, 65);
 }
+
+// new code
+.search_section_background {
+  display: flex;
+  background-color: #01d1bb;
+  width: 100%;
+  height: calc(100vh / 1080px * 220px);
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+//search_section_background_left
+
+.search_section_topic {
+  cursor: default;
+  position: relative;
+  font-size: 23px;
+  font-weight: 600;
+  margin-top: 30px;
+  margin-left: 25px;
+}
+.search_section_button {
+  cursor: pointer;
+  background-color: #00bdc8;
+  border-radius: 18px;
+  border-style: solid;
+  border-color: hsl(198, 100%, 20%);
+  border-width: 1px;
+  width: calc(1536px / 1920px * 143px);
+  height: calc(100vh / 1080px * 40px);
+  position: absolute;
+  margin-top: 100px;
+  margin-left: 25px;
+  color: white;
+  text-align: center;
+  line-height: calc(100vh / 1080px * 40px);
+  letter-spacing: 2px;
+}
+
+//search_section_background_right
+.search_section {
+  display: flex;
+  background-color: #00b6cc;
+  position: absolute;
+  top: 12px;
+  right: 15px;
+  width: calc(1536px / 1920px * 523px);
+  height: calc(100vh / 1080px * 186px);
+  border-radius: 7px;
+}
+
+//left
+.search_section_left {
+  width: calc(1536px / 1920px * 483px / 2);
+  height: 86%;
+  margin-top: 10px;
+  margin-left: 10px;
+}
+.search_section_left h2{
+  cursor:default;
+  color:white;
+  margin-top:-5px;
+  font-weight:10;
+  letter-spacing:1px;
+  font-size:40px;
+}
+.search_section_left p{
+  opacity:0.75;
+  position:absolute;
+  left:20px;
+  color:white;
+  height: calc(100vh / 1080px * 45px);
+  font-size:8px; 
+}
+#search_section_left_posts{
+  position:absolute;
+  margin-top:10px;
+
+}
+#search_section_left_hots{
+  position:absolute;
+  margin-top:25px;
+
+}
+#search_section_left_news{
+  position:absolute;
+  margin-top:40px;
+
+}
+//right
+.search_section_right {
+  width: calc(1536px / 1920px * 245px);
+  height: 86%;
+  margin-top: 10px;
+  margin-left: 10px;
+  //overflow:hidden;   可以用注释里面的部分来进行图片裁剪，但是可能只适用于部分图片
+}
+.search_section_right img {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  //top:-40px;
+}
+
+//searchList
+.searchList_item{
+  width:95%;
+  margin:0 auto;
+  margin-top:5px;
+  height: calc(100vh / 1080px * 100px);
+  border-style:solid;
+  border-width:1px;
+  border-color:transparent transparent #808080 transparent;
+  padding-bottom:20px;
+}
+.item_topic{
+  display:flex;
+  position:relative;
+  margin-top:10px;
+  height:22px;
+  left:30px;
+  img {
+   margin-left:5px;
+   width:30px;
+   height:19px;
+  }
+}
+.item_label{
+  display:flex;
+  position:relative;
+  margin-top:10px;
+  width:100%;
+  height:22px;
+  background-color:whitesmoke;
+}
+
 </style>
